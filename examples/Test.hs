f0f89246b246c53dfe49817e5e18d50c9d101ae2
Sylvain HENRY <sylvain.henry@inria.fr>
2012-07-12 17:58:42 +0200

    Show platform info

--- examples/Test.hs
+++ examples/Test.hs
@@ -1,7 +1,12 @@
 import ViperVM
 
+import Data.Traversable
+
 main = do
-  putStrLn "Hello"
-  cl <- loadOpenCL "/usr/lib/libOpenCL.so"
-  n <- getNumPlatforms cl
-  putStrLn $ show $ n
+  let cllib = "/usr/lib/libOpenCL.so"
+  putStrLn $ "Loading OpenCL library: " ++ cllib
+  cl <- loadOpenCL cllib
+  platforms <- clGetPlatformIDs cl
+  platformNames <- traverse (\x -> clGetPlatformInfo cl x CL_PLATFORM_NAME) platforms
+  platformVendors <- traverse (\x -> clGetPlatformInfo cl x CL_PLATFORM_VENDOR) platforms
+  traverse (putStrLn) (zipWith (\x y -> x ++ " - " ++ y) platformNames platformVendors)
