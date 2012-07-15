a43db0e742fc06bc9868b865db2806ecdb42dd57
Sylvain HENRY <hsyl20@gmail.com>
2012-07-15 18:32:10 +0200

    Export OpenCL modules

--- ViperVM/Backends/OpenCL.hs
+++ ViperVM/Backends/OpenCL.hs
@@ -1,7 +1,11 @@
 module ViperVM.Backends.OpenCL (
   module ViperVM.Backends.OpenCL.Loader,
-  module ViperVM.Backends.OpenCL.Query
+  module ViperVM.Backends.OpenCL.Query,
+  module ViperVM.Backends.OpenCL.Context,
+  module ViperVM.Backends.OpenCL.Memory
 ) where
 
 import ViperVM.Backends.OpenCL.Loader
 import ViperVM.Backends.OpenCL.Query
+import ViperVM.Backends.OpenCL.Context
+import ViperVM.Backends.OpenCL.Memory
--- examples/Test.hs
+++ examples/Test.hs
@@ -6,7 +6,11 @@ main = do
   let cllib = "/usr/lib/libOpenCL.so"
   putStrLn $ "Loading OpenCL library: " ++ cllib
   cl <- loadOpenCL cllib
+
+  -- Show platforms
   platforms <- clGetPlatformIDs cl
   platformNames <- traverse (\x -> clGetPlatformInfo cl x CL_PLATFORM_NAME) platforms
   platformVendors <- traverse (\x -> clGetPlatformInfo cl x CL_PLATFORM_VENDOR) platforms
   traverse (putStrLn) (zipWith (\x y -> x ++ " - " ++ y) platformNames platformVendors)
+
+  -- Create context
