0e8207e671f695a32b2542ef12eb2d121c9037f2
Sylvain HENRY <hsyl20@gmail.com>
2012-07-15 18:38:03 +0200

    Show devices

--- examples/Test.hs
+++ examples/Test.hs
@@ -1,5 +1,4 @@
 import ViperVM
-
 import Data.Traversable
 
 main = do
@@ -8,9 +7,17 @@ main = do
   cl <- loadOpenCL cllib
 
   -- Show platforms
+  putStrLn "Platforms:"
   platforms <- clGetPlatformIDs cl
   platformNames <- traverse (\x -> clGetPlatformInfo cl x CL_PLATFORM_NAME) platforms
   platformVendors <- traverse (\x -> clGetPlatformInfo cl x CL_PLATFORM_VENDOR) platforms
   traverse (putStrLn) (zipWith (\x y -> x ++ " - " ++ y) platformNames platformVendors)
+  putStrLn "-------------"
 
-  -- Create context
+  -- Show devices
+  putStrLn "Devices:"
+  devices <- traverse (\x -> clGetDeviceIDs cl x CL_DEVICE_TYPE_ALL) platforms
+  deviceNames <- traverse (traverse (clGetDeviceName cl)) devices
+  traverse (traverse (putStrLn)) deviceNames
+  putStrLn "-------------"
+
