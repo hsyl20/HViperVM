e49961adfb67a2571b10b3bb1cd1fc7f30883a8b
Sylvain HENRY <sylvain.henry@inria.fr>
2012-08-01 15:13:49 +0200

    Doc

--- ViperVM/Scheduling/TaskManager.hs
+++ ViperVM/Scheduling/TaskManager.hs
@@ -15,14 +15,21 @@ taskManagerScheduler :: Scheduler
 taskManagerScheduler (SubmitTask task r) = do
   
   let Task (KernelSet ki _) ds = task
+
+  -- Make kernel parameters from inputs:
+  --   - data accessed in read-only mode
+  --   - data accessed in read-write mode
+  --   - data that must be allocated
   let params = makeParameters ki ds
 
+  -- Retrieve or create data for each parameter
   datas <- traverse paramToData params
 
+  -- Store task and data in state
   modify (submittedTasks ^%= (:) (task,datas))
 
+  -- Return result data
   let result = makeResult ki datas
-
   setEventR r result
 
 taskManagerScheduler _ = voidR
