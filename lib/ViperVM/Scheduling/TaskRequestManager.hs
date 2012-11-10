ff9af29e25319852498701a867836780770fcdd9
Sylvain HENRY <hsyl20@gmail.com>
2012-11-10 05:05:36 +0100

    Add doc

--- lib/ViperVM/Scheduling/TaskRequestManager.hs
+++ lib/ViperVM/Scheduling/TaskRequestManager.hs
@@ -13,20 +13,27 @@ import Data.Maybe (catMaybes)
 import qualified Data.Map as Map
 import qualified Data.List as List
 
+-- | Maintain the list of requests that need to be fulfilled to execute a scheduled task
 taskRequestManager :: Scheduler
 
+-- When a task is scheduled on a proc, we determine its initial request set
 taskRequestManager (TaskScheduled task proc) = do
   requests <- determineTaskRequests proc task
   storeTaskRequestsR task requests
 
+-- When a kernel has been compiled for several procs, we store the compiled kernels
+-- and we remove compilation requests that have been fulfilled
 taskRequestManager (KernelCompiled k procs cks) = do
   traverse_ (uncurry $ storeCompiledKernelR k) $ catMaybes $ (\(ck,p) -> fmap (,p) ck) <$> zip cks procs
   updateCompilationRequestsR
   
+-- When an invalid data instance is allocated, we store it and we update allocation requests
 taskRequestManager (DataAllocated d di) = do
   modify (invalidDataInstances ^%= Map.insertWith (++) d [di])
   updateAllocationRequestsR
 
+-- When a transfer has completed to initialize a data, we indicate that the latter is
+-- no longer invalid, we store the new data and we update transfer requests
 taskRequestManager (DataTransfered d di) = do
   modify (invalidDataInstances ^%= Map.adjust (List.delete di) d)
   modify (datas ^%= Map.insertWith (++) d [di])
