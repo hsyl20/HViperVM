5ef110354344223250ca47750c5047c454c03432
Sylvain HENRY <hsyl20@gmail.com>
2013-01-26 15:44:43 +0100

    Add DataManager module

---
+++ lib/ViperVM/Internals/RegionManager.hs
@@ -0,0 +1,37 @@
+module ViperVM.Internals.RegionManager (
+   ViperVM.Internals.RegionManager.init,
+   add,
+   remove,
+   regions
+) where
+
+import Data.Map.Lazy
+import qualified Data.Map.Lazy as Map
+import Data.Maybe
+import qualified Data.List as List
+
+import ViperVM.Buffer
+import ViperVM.Region
+
+-- | Manage buffer regions
+data RegionManager = RegionManager (Map Buffer [Region])
+
+-- | Initialize a region manager
+init :: RegionManager
+init = RegionManager Map.empty
+
+-- | Get regions 
+regions :: RegionManager -> Buffer -> [Region]
+regions (RegionManager m) b = fromMaybe [] (Map.lookup b m)
+
+-- | Add a region
+add :: RegionManager -> Buffer -> Region -> RegionManager
+add (RegionManager m) b r = RegionManager $ Map.update (wrap . ((:) r )) b m
+   where
+      wrap s = if List.null s then Nothing else Just s
+
+-- | Remove a region
+remove :: RegionManager -> Buffer -> Region -> RegionManager
+remove (RegionManager m) b r = RegionManager $ Map.update (wrap . (List.delete r)) b m
+   where
+      wrap s = if List.null s then Nothing else Just s
