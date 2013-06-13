66db3f6b317f3194e9859ea32b5412f10cc49944
Sylvain HENRY <hsyl20@gmail.com>
2013-06-13 14:00:37 +0200

    Implement indirect transfers through the host memory

--- lib/ViperVM/Platform/SharedObjectManager.hs
+++ lib/ViperVM/Platform/SharedObjectManager.hs
@@ -8,6 +8,7 @@ module ViperVM.Platform.SharedObjectManager (
 ) where
 
 import ViperVM.Platform.Platform
+import ViperVM.Platform.Link
 import ViperVM.Platform.Object
 import ViperVM.Platform.Memory
 import ViperVM.Platform.SharedObject
@@ -15,6 +16,7 @@ import ViperVM.Platform.ObjectManager
 
 import Control.Concurrent.STM
 import Control.Applicative ( (<$>) )
+import Control.Monad (foldM)
 import Data.Set as Set
 
 data SharedObjectManager = SharedObjectManager {
@@ -41,26 +43,41 @@ allocateInstance som so mem = allocateFromDescriptor om mem desc
 -- | Allocate a new instance, transfer appropriate data from another one, then
 -- associate the new instance
 allocateTransferAttach :: SharedObjectManager -> SharedObject -> Object -> Memory -> IO Object
-allocateTransferAttach som so src mem = do
-   let om = objectManager som
+allocateTransferAttach som so src dstMem = do
+   let 
+      om = objectManager som
+      srcMem = objectMemory src
+      pf = getSharedObjectManagerPlatform som
+      lks = linksBetween srcMem dstMem (links pf)
+      indirectTransfer = Prelude.null lks
 
-   -- Allocate
-   dst <- allocateInstance som so mem
+   -- Allocate destination
+   dst <- allocateInstance som so dstMem
+
+   -- Intermediate steps
+   steps <- if indirectTransfer
+               then do
+                  ho <- allocateInstance som so HostMemory -- Use host memory as intermediate
+                  return [ho,dst]
+               else return [dst]
+
+   let 
+      f s d = do
+         transferObject om s d
+         atomically (attachObject so d)
+         return d
 
    -- Perform transfer
-   transferObject om src dst
-
-   -- Attach 
-   atomically (attachObject so dst)
-
-   return dst
+   foldM f src steps
 
 
 -- | Ensure that an instance of a shared object is in a given memory. perform a transfer if necessary
 ensureInMemory :: SharedObjectManager -> Memory -> SharedObject -> IO Object
 ensureInMemory som mem so = do
    Set.elems <$> atomically (memoryObjects mem so) >>= \case
+
       x:_ -> return x
+
       [] -> Set.elems <$> atomically (objects so) >>= \case
          [] -> error "Uninitialized object accessed in read-only mode"
          src:_ -> allocateTransferAttach som so src mem
